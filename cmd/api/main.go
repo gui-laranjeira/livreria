21d4ccf86b9b675df2b7b32be1c1080aa1e7fa26
Guilherme <guilr14@gmail.com>
2025-05-01 11:20:33 -0300

    feat: add database connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gui-laranjeira/livreria/configs"
+	"github.com/gui-laranjeira/livreria/internal/core/infrastructure/database"
 	"github.com/gui-laranjeira/livreria/internal/core/router"
 )
 
 func main() {
 	r := gin.Default()
 	router.SetupRoutes(r)
+
+	dbConfig, err := configs.LoadDBConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = database.OpenConnection(dbConfig)
+	if err != nil {
+		panic(err)
+	}
+
 	if err := r.Run(); err != nil {
 		panic(err)
 	}
---
+++ internal/core/infrastructure/database/db.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/gui-laranjeira/livreria/configs"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func OpenConnection(cfg *configs.DBConfig) (*sql.DB, error) {
+	connStr := fmt.Sprintf(
+		"postgres://%v:%v@%v:%v/%v?sslmode=disable",
+		cfg.User,
+		cfg.Pass,
+		cfg.Container,
+		cfg.Port,
+		cfg.Database,
+	)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Error opening database connection:", err)
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		log.Fatal("Error pinging database:", err)
+		return nil, err
+	}
+	return db, nil
+}
